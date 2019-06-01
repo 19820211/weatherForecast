c149e660ffde2d4d03b40200cec8ced5ccb91e83
Grzegorz Stepien <Grzegorz>
2019-06-01 13:54:01 +0200

    Added form

--- src/main/java/com/example/weatherForecast/HomeController.java
+++ src/main/java/com/example/weatherForecast/HomeController.java
@@ -7,12 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home(ModelMap map){
-        map.put("a",20);
-        return "home";
-    }
+//    @GetMapping("/")
+//    public String home(ModelMap map){
+//        map.put("a",20);
+//        return "home";
+//    }
 
+    @GetMapping("/")
+    public String getForm(){
+        return "weather";
+    }
 
 
 }
